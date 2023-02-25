474af00483074075968326795f28ec6115f5695a
rblevi01 <r.scott.blevins@gmail.com>
2023-02-25 17:23:17 -0500

    Get rid of JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # This code normalizes a JSON response obtained from the Fruityvice API using the pandas library in Python.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
