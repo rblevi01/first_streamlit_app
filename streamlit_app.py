ebec7f31f3f230be6fc62f4b45cfc2af270b1102
rblevi01 <r.scott.blevins@gmail.com>
2023-02-25 17:22:31 -0500

    Normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# This code normalizes a JSON response obtained from the Fruityvice API using the pandas library in Python.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# normalize
+streamlit.dataframe(fruityvice_normalized)
