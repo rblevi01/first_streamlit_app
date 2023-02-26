29fc6a5d0c2c5d391a1cb29dab6b619e0ee22a67
rblevi01 <r.scott.blevins@gmail.com>
2023-02-26 13:37:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -40,6 +41,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # normalize
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
